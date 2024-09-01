b04e70cffd052850630ba42b8401ef4f4b938ff3
MafasAhmath <mafasathirms2@gmail.com>
2024-09-01 23:09:26 +0530

    create regForm

---
+++ angularTest/src/app/regform/regform.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-regform',
+  standalone: true,
+  imports: [],
+  templateUrl: './regform.component.html',
+  styleUrl: './regform.component.scss'
+})
+export class RegformComponent {
+
+}
